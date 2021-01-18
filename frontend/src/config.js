ac973c773d748f095fab1a9be36bfbade3f3325e
Edgar Coime <e.coimeii@gmail.com>
2021-01-18 12:37:07 -0800

    changed docker-compose to comply with new UserManual instructions

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 var API_URL = "http://localhost:3001"
 if (process.env.NODE_ENV === "production") {
-  API_URL = "http://dev.mylivingcity.org/api"
+  API_URL = process.env.PRODUCTION_API
 }
 
 module.exports = API_URL
