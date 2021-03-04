63ac19d0d03d1df8c880e57a8a7a34a79994d4c0
Edgar Coime <e.coimeii@gmail.com>
2021-03-03 16:29:35 -0800

    feat: add getAll ideas query

--- server/controllers/idea.js
+++ server/controllers/idea.js
@@ -1,4 +1,5 @@
 const passport = require('passport');
+const { PrismaClient, Prisma } = require('@prisma/client')
 
 const express = require('express');
 const ideaRouter = express.Router();
@@ -15,6 +16,28 @@ ideaRouter.get(
 				message: error.message,
 				stack: error.stack,
 			})
+    }
+  }
+)
+
+ideaRouter.get(
+  '/getall',
+  async (req, res, next) => {
+    const prisma = new PrismaClient({ log: [ 'query' ]})
+    try {
+      const allIdeas = await prisma.idea.findMany();
+
+      res.status(200).json(allIdeas);
+    } catch (error) {
+      res.status(400).json({
+        message: "An error occured while trying to fetch all ideas",
+        details: {
+          errorMessage: error.message,
+          errorStack: error.stack,
+        }
+      });
+    } finally {
+      await prisma.$disconnect();
     }
   }
 )
