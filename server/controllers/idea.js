830445092e5abea65cc60abc45445b99183fef22
Edgar Coime <e.coimeii@gmail.com>
2021-03-03 17:12:33 -0800

    feat: add create idea query based on JWT

--- server/controllers/idea.js
+++ server/controllers/idea.js
@@ -1,5 +1,5 @@
 const passport = require('passport');
-const { PrismaClient, Prisma } = require('@prisma/client')
+const { PrismaClient } = require('@prisma/client')
 
 const express = require('express');
 const ideaRouter = express.Router();
@@ -31,6 +31,55 @@ ideaRouter.get(
     } catch (error) {
       res.status(400).json({
         message: "An error occured while trying to fetch all categories",
+        details: {
+          errorMessage: error.message,
+          errorStack: error.stack,
+        }
+      });
+    } finally {
+      await prisma.$disconnect();
+    }
+  }
+)
+
+ideaRouter.post(
+  '/create',
+  passport.authenticate('jwt', { session: false }),
+  async (req, res, next) => {
+    const prisma = new PrismaClient({ log: [ 'query' ]})
+    try {
+      // passport middleware provides this based on JWT
+      const { email, id } = req.user;
+      const { categoryId } = req.body;
+
+      // Check if category id is added
+      if (!categoryId) {
+        return res.status(400).json({
+          message: 'An Idea must be under a specific category.',
+          details: {
+            errorMessage: 'Creating an idea must be explicityly supplied with a "categoryId" field.',
+            errorStack: '"CategoryId" must be defined in the body with a valid id found in the database.',
+          }
+        })
+      }
+
+      const ideaData = {
+        ...req.body,
+        authorId: id
+      };
+
+      // Create an idea and make the author JWT bearer
+      const createdIdea = await prisma.idea.create({
+        data: ideaData
+      });
+
+      res.status(201).json({
+        createdIdea
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(400).json({
+        message: "An error occured while trying to create an Idea.",
         details: {
           errorMessage: error.message,
           errorStack: error.stack,
