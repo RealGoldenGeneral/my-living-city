2d2ce2899879fe52013971e0616c81390380cb00
Edgar Coime <e.coimeii@gmail.com>
2021-03-30 16:12:43 -0700

    fix: reset database

--- server/prisma/migrations/20210330231233_initial/migration.sql
+++ server/prisma/migrations/20210330231233_initial/migration.sql
@@ -1,5 +1,71 @@
 -- CreateEnum
+CREATE TYPE "user_type" AS ENUM ('USER', 'ADMIN', 'DEVELOPER');
+
+-- CreateEnum
 CREATE TYPE "idea_state" AS ENUM ('IDEA', 'PROPOSAL', 'PROJECT');
+
+-- CreateTable
+CREATE TABLE "report" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserRole" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "user_geo" (
+    "id" SERIAL NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "lat" DECIMAL(65,30),
+    "lon" DECIMAL(65,30),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "user_address" (
+    "id" SERIAL NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "street_address" TEXT,
+    "street_address_2" TEXT,
+    "city" TEXT,
+    "country" TEXT,
+    "postal_code" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "user" (
+    "id" TEXT NOT NULL,
+    "user_role_id" INTEGER,
+    "user_type" "user_type" NOT NULL DEFAULT E'USER',
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "f_name" TEXT,
+    "l_name" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY ("id")
+);
 
 -- CreateTable
 CREATE TABLE "category" (
@@ -16,8 +82,8 @@ CREATE TABLE "category" (
 CREATE TABLE "idea_geo" (
     "id" SERIAL NOT NULL,
     "idea_id" INTEGER NOT NULL,
-    "lat" DECIMAL(65,30) NOT NULL,
-    "lon" DECIMAL(65,30) NOT NULL,
+    "lat" DECIMAL(65,30),
+    "lon" DECIMAL(65,30),
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
@@ -28,11 +94,11 @@ CREATE TABLE "idea_geo" (
 CREATE TABLE "idea_address" (
     "id" SERIAL NOT NULL,
     "idea_id" INTEGER NOT NULL,
-    "street_address" TEXT NOT NULL,
-    "street_address_2" TEXT NOT NULL,
-    "city" TEXT NOT NULL,
-    "country" TEXT NOT NULL,
-    "postal_code" TEXT NOT NULL,
+    "street_address" TEXT,
+    "street_address_2" TEXT,
+    "city" TEXT,
+    "country" TEXT,
+    "postal_code" TEXT,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
@@ -120,6 +186,15 @@ CREATE TABLE "idea_comment_like" (
 );
 
 -- CreateIndex
+CREATE UNIQUE INDEX "user_geo_user_id_unique" ON "user_geo"("user_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_address_user_id_unique" ON "user_address"("user_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user.email_unique" ON "user"("email");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "idea_geo_idea_id_unique" ON "idea_geo"("idea_id");
 
 -- CreateIndex
@@ -130,6 +205,15 @@ CREATE UNIQUE INDEX "proposal_idea_id_unique" ON "proposal"("idea_id");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "project_idea_id_unique" ON "project"("idea_id");
+
+-- AddForeignKey
+ALTER TABLE "user_geo" ADD FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "user_address" ADD FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "user" ADD FOREIGN KEY ("user_role_id") REFERENCES "UserRole"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "idea_geo" ADD FOREIGN KEY ("idea_id") REFERENCES "idea"("id") ON DELETE CASCADE ON UPDATE CASCADE;
