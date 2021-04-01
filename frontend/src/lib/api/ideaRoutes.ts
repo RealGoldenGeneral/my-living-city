0338086fb72776fab176b2b4c722da0e8dd9080c
Edgar Coime <e.coimeii@gmail.com>
2021-04-01 11:49:07 -0700

    feat: add get category hook and createIdea route

--- frontend/src/lib/api/ideaRoutes.ts
+++ frontend/src/lib/api/ideaRoutes.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { API_BASE_URL } from "../constants";
 import { IIdea } from "../types/data/idea.type";
+import { CreateIdeaInput } from "../types/input/createIdea.input";
 
 export const getAllIdeas = async () => {
   const res = await axios.get<IIdea[]>(`${API_BASE_URL}/idea/getall`);
@@ -9,6 +10,30 @@ export const getAllIdeas = async () => {
 
 export const getSingleIdea = async (ideaId: string) => {
   const res = await axios.get<IIdea>(`${API_BASE_URL}/idea/get/${ideaId}`);
+  return res.data;
+}
+
+export const postCreateIdea = async (ideaData: CreateIdeaInput, token: string | null) => {
+  // Parse data and data checking
+  const { categoryId, title, description} = ideaData;
+  const parsedCatId = Number(categoryId);
+
+  if (!parsedCatId || !title || !description) {
+    throw new Error('You must choose a category, define a title, and description of your idea.');
+  }
+
+  if (!token) {
+    throw new Error('Your session has expired. Please relogin and try again.');
+  }
+
+  const parsedPayload = {
+    ...ideaData,
+    categoryId: parsedCatId,
+  }
+
+  const res = await axios.post<IIdea>(`${API_BASE_URL}/idea/create`, parsedPayload, {
+    headers: { "secret_token": token }
+  });
   return res.data;
 }
 
--- frontend/src/lib/utilityFunctions.ts
+++ frontend/src/lib/utilityFunctions.ts
@@ -1,4 +1,5 @@
 import { IUser } from "./types/data/user.type";
+import { FetchError } from "./types/types";
 
 /**
  * Stringifies given Object and stores it in local storage using the 
@@ -15,10 +16,64 @@ export const storeObjectInLocalStorage = (
   localStorage.setItem(localStorageKey, stringifiedObj);
 }
 
+/**
+ * A utility function that stores a given user object and token in local storage
+ * @param token A valid token retrieved from register or login and stored in memory
+ * @param user A user object that will be stored in memory
+ */
 export const storeUserAndTokenInLocalStorage = (
   token: string,
   user: IUser,
 ): void => {
   storeObjectInLocalStorage('logged-user', user);
   localStorage.setItem('token', token);
+}
+
+/**
+ * Capitalize the first letter of a string.
+ * @param s A string to capitalize
+ * @returns A new string with the first character capitalized
+ */
+export const capitalizeString = (s: string) => {
+  return s.charAt(0).toUpperCase() + s.slice(1);
+}
+
+/**
+ * Error handling function that parses a potential Axios error that may be thrown when submitting 
+ * new data.
+ * 
+ * @param genericMessage Fallback error message if no specific message is received from server
+ * @param error An error object thrown that could be any
+ * @returns {FetchError} Error details
+ */
+export const handlePotentialAxiosError = (
+  genericMessage: string, 
+  error: any,
+): FetchError => {
+  let errorObj: FetchError = {
+    message: ''
+  }
+  if (error.response) {
+    // Request made and server responded
+    errorObj.message = error.response.data.message;
+    console.log(error.response.data);
+    console.log(error.response.status);
+    console.log(error.response.headers);
+  } else if (error.request) {
+    // The request was made but no response was received
+    errorObj.message = "Error no response received"
+    console.log(error.request);
+  } else {
+    // Something happened in setting up the request that triggered an Error
+    errorObj.message = error.message;
+    console.log('Error', error.message);
+  }
+
+  if (!errorObj.message) {
+    errorObj.message = genericMessage;
+  }
+
+  console.log(error);
+
+  return errorObj;
 }
