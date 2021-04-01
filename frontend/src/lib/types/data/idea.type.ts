5f8fb1247a533362dcc3d8f35c58660a4ddb898a
Edgar Coime <e.coimeii@gmail.com>
2021-04-01 10:03:33 -0700

    feat: add and update idea type

--- frontend/src/lib/types/data/idea.type.ts
+++ frontend/src/lib/types/data/idea.type.ts
@@ -1,3 +1,10 @@
+import { Address } from "node:cluster";
+import { Category } from "./category.type";
+import { Geo } from "./geo.type";
+import { Proposal } from "./proposal.type";
+import { Project } from "./project.type";
+import { Rating } from "./rating.type";
+
 export interface IIdea {
 	id: number;
 	authorId: string;
@@ -13,4 +20,15 @@ export interface IIdea {
 	active: boolean;
 	createdAt: Date;
 	updatedAt: Date;
+
+	// Relationships can be nullable 
+	geo?: Geo;
+	address?: Address;
+	category?: Category;
+
+	// Extended Relationships
+	comments?: Comment[];
+	ratings?: Rating[];
+	proposalInfo?: Proposal;
+	projectInfo?: Project;
 }
