0f9b1a8feece80d4f708bb31a1796ea42801257f
Edgar Coime <e.coimeii@gmail.com>
2021-04-26 09:44:09 -0700

    docs: add comments for util functions

--- server/lib/sortingFunctions.js
+++ server/lib/sortingFunctions.js
@@ -1,4 +1,15 @@
 
+/**
+ * Array.prototype.sort predicate that will only sort commentObjects from prisma 
+ * 
+ * REMEMBER: If prisma comment structure does not contain _count value in object this
+ * function will not work and will most likely cause route to crash and throw error. 
+ * Change the sort predicate if schema is changed.
+ * 
+ * @param { commentObject } a A comment object from prisma to compare
+ * @param { commentObject } b A comment object from prisma to compare
+ * @returns A -1, 0, or 1 value that determines sort order
+ */
 const compareCommentsBasedOnLikesAndDislikes = (a, b) => {
   // TODO: Will have to change if naming convention changes as well
   const { _count: aCount } = a;
