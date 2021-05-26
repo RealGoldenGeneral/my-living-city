9b9becd9fa1932e9b53b87e2b0c3ab4046294ad4
Lengfeng Yu <1760870550yu@gmail.com>
2021-05-25 23:25:00 -0700

    segment creation api scratch completed

--- server/controllers/segment.js
+++ server/controllers/segment.js
@@ -11,7 +11,9 @@ segmentRouter.post(
     passport.authenticate('jwt',{session:false}),
     async(req,res) => {
         try{
-            let errorMessage,errorStack;
+            let error = '';
+            let errorMessage = '';
+            let errorStack = '';
             //get email and user id from request
             const { email, id } = req.user;
             //find the requesting user in the database
@@ -19,9 +21,62 @@ segmentRouter.post(
                 where:{id:id},
                 select:{userType:true}
             });
-
+            //User must be admin to create segment
             if (theUser.userType == 'ADMIN'){
+                const {contry,province,name,superSegName} = req.body;
 
+                //if there's no object in the request body
+                if(isEmpty(req.body)){
+                    return res.status(400).json({
+                        message: 'The objects in the request body are missing',
+                        details: {
+                            errorMessage: 'Creating a segment must supply necessary fields explicitly.',
+                            errorStack: 'necessary fields must be provided in the body with valid values',
+                        }
+                    })
+                }
+                //if country field is missing
+                if(!contry){
+                    error+='A segment must has a country field. ';
+                    errorMessage+='Creating a segment must explicitly be supplied with a country field. ';
+                    errorStack+='cuntry must be provided in the body with a valid value. ';
+                }
+
+                //if province is missing
+                if(!province){
+                    error+='A segment must has a province field. ';
+                    errorMessage+='Creating a segment must explicitly be supplied with a province field. ';
+                    errorStack+='province must be provided in the body with a valid value. ';
+                }
+
+                if(!name){
+                    error+='A segment must has a name field. ';
+                    errorMessage+='Creating a segment must explicitly be supplied with a name field. ';
+                    errorStack+='name must be provided in the body with a valid value. ';
+                }
+
+                //If there's error in error holder
+                if(error||errorMessage||errorStack){
+                    return res.status(400).json({
+                        message: error,
+                        details: {
+                          errorMessage: errorMessage,
+                          errorStack: errorStack
+                        }
+                    });
+                }
+
+                //create a segement table item
+                const createSegment = await prisma.segments.create({
+                    data:{
+                        country:contry,
+                        province:province,
+                        name:name,
+                        superSegName:superSegName
+                    }
+                })
+
+                res.status(200).json(createSegment);
             }else{
                 return res.status(403).json({
                     message: "You don't have the right to add a segment!",
