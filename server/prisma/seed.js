4cad1b8243d00ca9dd0664f23f5e1e5842e7a212
Lengfeng Yu <1760870550yu@gmail.com>
2021-06-22 12:23:56 -0700

    seed.js segment population scratch completed

--- server/prisma/seed.js
+++ server/prisma/seed.js
@@ -20,6 +20,28 @@ async function main() {
 		'worker' 
 	];
 
+	const defaultSuperSegment = {
+		name:"CRD",
+		country: "Canada",
+		province: "BC"
+	}
+
+	const defaultSegments = [
+		{superSegId:0,country: 'canada', province: 'british columbia', name: 'victoria', superSegName: 'crd'},
+		{superSegId:0,country: 'canada', province: 'british columbia', name: 'saanich', superSegName: 'crd'},
+		{superSegId:0,country: 'canada', province: 'british columbia', name: 'esquimalt', superSegName: 'crd'},
+	];
+
+	const defaultSubSegments = [
+		//victoria
+		{segId: 0, name:'fairfield', lat: 0, lon: 0},
+		//saanich
+		{segId: 1, name:'rutledge park', lat: 0, lon: 0},
+		//esquimalt
+		{segId: 2, name:'saxe point', lat: 0, lon: 0},
+		
+	];
+
 	const resolvedCategories = await Promise.all(
 		defaultIdeaCategories.map(({ title, description }) => (
 			prisma.category.upsert({
@@ -52,10 +74,73 @@ async function main() {
 				}
 			})
     ))
-	)
+	);
+
+	const createSuperSegment = await Promise.all(
+		prisma.superSegment.upsert({
+			where:{
+				name:defaultSuperSegment.name
+			},
+			update:{
+				name:defaultSuperSegment.name,
+				country:defaultSuperSegment.country,
+				province:defaultSuperSegment.province
+			},
+			create:{
+				name:defaultSuperSegment.name,
+				country:defaultSuperSegment.country,
+				province:defaultSuperSegment.province
+			}
+		})
+	);
+
+	const createSegments = await Promise.all(
+		defaultSegments.map((segment) => {
+			await prisma.segments.upsert({
+				where:{name:segment.name},
+				update:{
+					superSegId:segment.superSegId,
+					country:segment.country,
+					province:segment.province,
+					name:segment.name,
+					superSegName:segment.superSegName
+				},
+				create:{
+					superSegId:segment.superSegId,
+					country:segment.country,
+					province:segment.province,
+					name:segment.name,
+					superSegName:segment.superSegName
+				}
+			})
+		})
+	);
+
+	const createSubSegments = await Promise.all(
+		defaultSubSegments.map((subSegment) => {
+			prisma.subSegments.upsert({
+				where:{name:subSegment.name},
+				update:{
+					segId:subSegment.segId,
+					name:subSegment.name,
+					lat:subSegment.lat,
+					lon:subSegment.lon
+				},
+				create:{
+					segId:subSegment.segId,
+					name:subSegment.name,
+					lat:subSegment.lat,
+					lon:subSegment.lon
+				}
+			})
+		})
+	);
 
 	console.log('Resolved populated Categories', resolvedCategories);
 	console.log('Resolved populated UserRoles', resolvedUserRoles);
+	console.log('Resolved populated Super Segment', createSuperSegment);
+	console.log('Resolved populated segments', createSegments);
+	console.log('Resolved populated sub segments', createSubSegments);
 }
 
 main()
