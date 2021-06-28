517b5ccf16278631a2637765eba0116f3e82e478
unknown <Mackdeluca@gmail.com>
2021-06-27 20:26:38 -0700

    feat: Started userSegment card in segment manager

---
+++ frontend/src/components/partials/UserSegmentCard.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import {Button, Card} from 'react-bootstrap';
+interface UserSegmentCardProps {
+
+}
+
+export const UserSegmentCard: React.FC<UserSegmentCardProps> = ({}) => {
+        return (
+        <Card>
+            <Card.Header>Segment and SubSegment Requests<Button className="float-right" size="sm">View Requests</Button></Card.Header>
+            <Card.Body>
+            <table className="table table-striped">
+            <thead>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                </tr>
+                <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+                </tr>
+                <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+                </tr>
+            </tbody>
+            </table>
+            </Card.Body>
+        </Card>
+        );
+}
