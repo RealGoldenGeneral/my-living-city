5cfe601ddd9cb3b3f09a61a1636c79953982fb3a
unknown <Mackdeluca@gmail.com>
2021-06-30 21:51:10 -0700

    fix: New usertype now works

--- server/prisma/migrations/20210701044932_new_user_type/migration.sql
+++ server/prisma/migrations/20210701044932_new_user_type/migration.sql
@@ -2,4 +2,4 @@
 ALTER TYPE "user_type" ADD VALUE 'NORMAL';
 
 -- AlterTable
-ALTER TABLE "user" ALTER COLUMN "user_type" SET DEFAULT 'NORMAL';
+ALTER TABLE "user" ALTER COLUMN "user_type" DROP DEFAULT;
