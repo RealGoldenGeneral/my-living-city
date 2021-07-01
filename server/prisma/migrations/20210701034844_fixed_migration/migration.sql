ec60e04d69de395f8024b457b35dce26d3c2d0cb
unknown <Mackdeluca@gmail.com>
2021-06-30 21:08:39 -0700

    fix: trying normal instead of default for usertype

--- server/prisma/migrations/20210701034844_fixed_migration/migration.sql
+++ server/prisma/migrations/20210701034844_fixed_migration/migration.sql
@@ -1,5 +1,5 @@
 -- AlterEnum
-ALTER TYPE "user_type" ADD VALUE 'DEFAULT';
+ALTER TYPE "user_type" ADD VALUE 'NORMAL';
 
 -- AlterTable
-ALTER TABLE "user" ALTER COLUMN "user_type" SET DEFAULT 'DEFAULT';
+ALTER TABLE "user" ALTER COLUMN "user_type" SET DEFAULT 'NORMAL';
