46266a20653b3b9fa9b3264e80053c5a4b03465f
unknown <Mackdeluca@gmail.com>
2021-07-07 12:47:09 -0700

    feat: added buttons for the userMangement tool

--- frontend/src/components/content/UserMangementContent.tsx
+++ frontend/src/components/content/UserMangementContent.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Card, Table, DropdownButton, Dropdown, Container } from 'react-bootstrap';
+import React, { useState } from 'react'
+import { Card, Table, DropdownButton, Dropdown, Container, Button } from 'react-bootstrap';
 import { IUser } from 'src/lib/types/data/user.type';
 
 interface UserMangementContentProps {
@@ -7,6 +7,8 @@ interface UserMangementContentProps {
 }
 
 export const UserMangementContent: React.FC<UserMangementContentProps> = ({users}) => {
+    const [showControls, setShowControls] = useState(true);
+    const [hideControls, setHideControls] = useState('');
         return (
             <Container>
             <h2 className="mb-4 mt-4">User Management</h2>
@@ -16,28 +18,30 @@ export const UserMangementContent: React.FC<UserMangementContentProps> = ({users
             <Table bordered hover>
             <thead>
                 <tr>
-                <th scope="col">UserId</th>
-                <th scope="col">User Type</th>
                 <th scope="col">Email</th>
                 <th scope="col">First</th>
                 <th scope="col">Last</th>
+                <th scope="col">User Type</th>
                 <th scope="col">Controls</th>
                 </tr>
             </thead>
             <tbody>
             {users?.map((req: IUser, index: number) => (
                 <tr key={req.id}>
-                    <td>{req.id}</td>
-                    <td>{req.userType}</td>
                     <td>{req.email}</td>
                     <td>{req.fname}</td>
                     <td>{req.lname}</td>
+                    <td>{req.userType}</td>
                     <td>
-                    <DropdownButton id="dropdown-basic-button" title="Controls">
-                    <Dropdown.Item href="#/action-1" onClick={()=>console.log('hello')}>Action</Dropdown.Item>
-                    <Dropdown.Item href="#/action-2" onClick={()=>console.log('hello')}>Modify</Dropdown.Item>
-                    <Dropdown.Item href="#/action-3" onClick={()=>console.log('hello')}>Ban</Dropdown.Item>
-                    </DropdownButton>
+                    {req.id !== hideControls ?
+                        <DropdownButton id="dropdown-basic-button" title="Controls" variant="outline-primary">
+                            <Dropdown.Item href="#/action-1" onClick={()=>setHideControls(req.id)}>Action</Dropdown.Item>
+                            <Dropdown.Item href="#/action-2" onClick={()=>console.log('hello')}>Edit</Dropdown.Item>
+                            <Dropdown.Item href="#/action-3" onClick={()=>console.log('hello')}>Ban</Dropdown.Item>
+                        </DropdownButton>
+                        : <><Button className="mr-2" size="sm">Save</Button><Button size="sm" variant="danger">Cancel</Button></>
+                    }
+
                     </td>
                 </tr>
                 ))}
