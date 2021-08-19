f16a0985018078a05f82129511d81f6a58d30886
unknown <Mackdeluca@gmail.com>
2021-08-19 09:27:20 -0700

    working map key

--- frontend/src/components/map/SimpleMap.tsx
+++ frontend/src/components/map/SimpleMap.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import GoogleMapReact from 'google-map-react';
 import Marker from '../map/Marker';
-import {GOOGLE_PHYSICAL_MAP_KEY} from '../../lib/constants'
+import {MAP_KEY} from '../../lib/constants'
 const DEFAULTGEO = {lat:48.4284,lng:-123.3656}
 var markers:any = {
   home: {lat: null, lon: null},
@@ -13,10 +13,11 @@ const SimpleMap = (props: any) => {
     var [center, setCenter]: any = useState(DEFAULTGEO);
     const [zoom, setZoom] = useState(12);
     var [marker, setMarker]:any = useState({lat: null, lon: null})
+    console.log(MAP_KEY)
     return (
         <div style={{ height: '40vh', width: '100%' }}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: GOOGLE_PHYSICAL_MAP_KEY }}
+          bootstrapURLKeys={{ key: MAP_KEY}}
           center={center}
           defaultZoom={zoom}
           onClick={(obj:any)=>{setMarker(marker = {lat:obj.lat,lon:obj.lng});
