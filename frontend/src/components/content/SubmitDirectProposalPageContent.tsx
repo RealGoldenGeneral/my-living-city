babf025fb9c2913896781417c84022adace38bfe
Maximus Joe <maximusjoe278@gmail.com>
2022-01-24 11:28:49 -0800

    added submit proposal page template

---
+++ frontend/src/components/content/SubmitDirectProposalPageContent.tsx
@@ -0,0 +1,308 @@
+import { useFormik } from 'formik';
+import React, { useContext, useEffect, useState } from 'react'
+import { Col, Container, Row, Form, Button, Alert, Image } from 'react-bootstrap'
+import { useHistory } from 'react-router-dom';
+// import { getUserHomeSegmentInfo, getUserSchoolSegmentInfo, getUserWorkSegmentInfo } from 'src/lib/api/userSegmentRoutes';
+import { API_BASE_URL } from 'src/lib/constants';
+import { ISegment, ISegmentData, ISubSegment } from 'src/lib/types/data/segment.type';
+import { UserProfileContext } from '../../contexts/UserProfile.Context';
+import { postCreateIdea } from '../../lib/api/ideaRoutes';
+import { ICategory } from '../../lib/types/data/category.type';
+import { ICreateIdeaInput } from '../../lib/types/input/createIdea.input';
+import { IFetchError } from '../../lib/types/types';
+import { capitalizeFirstLetterEachWord, capitalizeString, handlePotentialAxiosError } from '../../lib/utilityFunctions';
+import { CONTENT, Toastie } from '../partials/LandingContent/CategoriesSection';
+import ImageUploader from 'react-images-upload';
+import ReactCrop from 'react-image-crop';
+import 'react-image-crop/dist/ReactCrop.css';
+import SubmitDirectProposalPage from 'src/pages/SubmitDirectProposalPage';
+interface SubmitDirectProposalPageContentProps {
+  categories: ICategory[] | undefined
+  segData: ISegmentData[];
+}
+
+/**
+ * Idea needs categoryId to submit
+ * default will be used if categories can't be fetched from server
+ */
+const DEFAULT_CAT_ID = 1;
+
+const SubmitDirectProposalPageContent: React.FC<SubmitDirectProposalPageContentProps> = ({ categories, segData}) => {
+  const { token, user } = useContext(UserProfileContext);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<IFetchError | null>(null);
+  const [crop, setCrop] = useState({aspect:16/9});
+  const history = useHistory();
+  const handleCommunityChange = (index: number) => {
+    if(segData[index].segType === 'Segment') {
+      formik.setFieldValue('segmentId',segData[index].id);
+      formik.setFieldValue('superSegmentId',undefined);
+      formik.setFieldValue('subSegmentId',undefined);
+    }
+    if(segData[index].segType === 'Sub-Segment') {
+      formik.setFieldValue('subSegmentId',segData[index].id);
+      formik.setFieldValue('superSegmentId',undefined);
+      formik.setFieldValue('segmentId',undefined);
+    }
+    if(segData[index].segType === 'Super-Segment'){
+      formik.setFieldValue('superSegmentId',segData[index].id);
+      formik.setFieldValue('subSegmentId',undefined);
+      formik.setFieldValue('segmentId',undefined);
+    }
+    formik.setFieldValue('userType', segData[index].userType);
+  }
+  const submitHandler = async (values: ICreateIdeaInput) => {
+    try {
+      // Set loading and error state
+      console.log(values);
+      setError(null);
+      setIsLoading(true);
+      setTimeout(() => console.log("timeout"), 5000);
+      const res = await postCreateIdea(values, user!.banned, token);
+      console.log(res);
+
+      setError(null);
+      history.push('/ideas/'+res.id);
+      formik.resetForm();
+    } catch (error) {
+      const genericMessage = 'An error occured while trying to create an Idea.';
+      const errorObj = handlePotentialAxiosError(genericMessage, error);
+      setError(errorObj);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+  // console.log(segment);
+  // console.log(subSegment);
+  const formik = useFormik<ICreateIdeaInput>({
+    initialValues: {
+      // TODO: CatId when chosen is a string
+      categoryId: categories ? categories[0].id : DEFAULT_CAT_ID,
+      title: '',
+      userType: segData ? segData[0].userType : 'Resident',
+      description: '',
+      artsImpact: '',
+      communityImpact: '',
+      energyImpact: '',
+      manufacturingImpact: '',
+      natureImpact: '',
+      address: {
+        streetAddress: '',
+        streetAddress2: '',
+        city: '',
+        postalCode: '',
+        country: '',
+      },
+      geo: {
+        lat: undefined,
+        lon: undefined,
+      },
+      segmentId: undefined,
+      subSegmentId: undefined,
+      superSegmentId: undefined
+    },
+    onSubmit: submitHandler
+  })
+  useEffect(() => {
+    if(segData){
+      handleCommunityChange(0);
+    }
+
+  },[])
+  return (
+    <Container className='submit-idea-page-content'>
+      <Row className='mb-4 mt-4 justify-content-center'>
+          <h2 className="pb-2 pt-2 display-6">Submit Direct Proposal</h2>
+      </Row>
+      <Row className='submit-idea-form-group justify-content-center'>
+        <Col lg={10} >
+          <Form onSubmit={formik.handleSubmit}>
+            <Form.Group controlId="submitIdeaCategory">
+              <h3 className="border-bottom mb-3">Direct Proposal Details</h3>
+              <Form.Label>Select Category:</Form.Label>
+              <Form.Control
+                as="select"
+                name="categoryId"
+                onChange={formik.handleChange}
+                value={formik.values.categoryId}
+              >
+                {categories && categories.map(cat => (
+                  <option 
+                    key={String(cat.id)} 
+                    value={Number(cat.id)}
+                    style={{
+                      textTransform: 'capitalize'
+                    }}
+                  >
+                    {capitalizeString(cat.title)}
+                  </option>
+                ))}
+              </Form.Control>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Select your community of interest</Form.Label>
+              <Form.Control
+                as='select'
+                type='number'
+                onChange={(e)=>handleCommunityChange(Number(e.target.value))}
+              >
+                {segData && segData.map((seg, index) => (
+                  <option key={String(seg.name)} value={index}>
+                    {capitalizeString(seg.name)}
+                  </option>
+                ))}
+                </Form.Control>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>What is the title of your proposal?</Form.Label>
+              <Form.Control
+                type='text'
+                name='title'
+                onChange={formik.handleChange}
+                value={formik.values.title}
+                placeholder="Enter the title of your propposal"
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Describe your proposal</Form.Label>
+              <Form.Control
+                as='textarea'
+                rows={3}
+                name='description'
+                onChange={formik.handleChange}
+                value={formik.values.description}
+              />
+            </Form.Group>
+            <Form.Group>
+                  <Form.Label>Proposal image</Form.Label>
+                  <ImageUploader name="imagePath" fileContainerStyle={{backgroundColor: "#F8F9FA"}}withPreview={true} onChange={(picture)=>{
+                    formik.setFieldValue('imagePath',picture);
+                    console.log(picture);
+                    }} imgExtension={['.jpg','.jpeg','.png','.webp']} buttonText="Select Proposal Image" maxFileSize={10485760} label={"Max file size 10mb, \n jpg, jpeg, png, webp"} singleImage={true}/>
+            </Form.Group>
+            <Form.Group>
+              <h3  className="border-bottom mb-3">Impact Areas</h3>
+              <Row className="align-items-end">
+              <Col xs={11}>
+              <Form.Control 
+                className='idea-impacts'
+                type='text'
+                name='communityImpact'
+                onChange={formik.handleChange}
+                value={formik.values.communityImpact}
+                placeholder='Community and Place'
+              />
+              </Col>
+              <Col>
+              <a href="javascript:void(0)"><Toastie header={CONTENT.community.header} subHeader={CONTENT.community.subHeader} body={CONTENT.community.body} img="/categories/MLC-Icons-Green-05.png" sizePercent="90%"/></a>
+              </Col>
+              </Row>
+              <Row className="align-items-end">
+              <Col xs={11}>
+              <Form.Control 
+                className='idea-impacts'
+                type='text'
+                name='natureImpact'
+                onChange={formik.handleChange}
+                value={formik.values.natureImpact}
+                placeholder='Nature and Food Security'
+              />
+              </Col>
+              <Col>
+              <a href="javascript:void(0)"><Toastie header={CONTENT.nature.header} subHeader={CONTENT.nature.subHeader} body={CONTENT.nature.body} img="/categories/MLC-Icons-Green-01.png" sizePercent="90%"/></a>
+              </Col>
+              </Row>
+              <Row className="align-items-end">
+              <Col xs={11}>
+              <Form.Control 
+                className='idea-impacts'
+                type='text'
+                name='artsImpact'
+                onChange={formik.handleChange}
+                value={formik.values.artsImpact}
+                placeholder='Arts, Culture, and Education'
+              />
+              </Col>
+              <Col>
+              <a href="javascript:void(0)"><Toastie header={CONTENT.arts.header} subHeader={CONTENT.arts.subHeader} body={CONTENT.arts.body} img="/categories/MLC-Icons-Green-04.png" sizePercent="90%"/></a>
+              </Col>
+              </Row>
+              <Row className="align-items-end">
+              <Col xs={11}>
+              <Form.Control 
+                className='idea-impacts'
+                type='text'
+                name='energyImpact'
+                onChange={formik.handleChange}
+                value={formik.values.energyImpact}
+                placeholder='Water and Energy'
+              />
+              </Col>
+              <Col>
+              <a href="javascript:void(0)"><Toastie header={CONTENT.water.header} subHeader={CONTENT.water.subHeader} body={CONTENT.water.body} img="/categories/MLC-Icons-Green-02.png" sizePercent="90%"/></a>
+              </Col>
+              </Row>
+              <Row className="align-items-end">
+              <Col xs={11}>
+              <Form.Control 
+                className='idea-impacts'
+                type='text'
+                name='manufacturingImpact'
+                onChange={formik.handleChange}
+                value={formik.values.manufacturingImpact}
+                placeholder='Manufacturing and Waste'
+              />
+              </Col>
+              <Col>
+              <a href="javascript:void(0)"><Toastie header={CONTENT.manufacturing.header} subHeader={CONTENT.manufacturing.subHeader} body={CONTENT.manufacturing.body} img="/categories/MLC-Icons-Green-03.png" sizePercent="90%"/></a>
+              </Col>
+              </Row>
+            </Form.Group>
+            <Form.Group>
+              <div className="checkbox">
+              <h3  className="border-bottom mb-3">Community Solicitation</h3>
+                  <div className="topping">
+                    <input type="checkbox" id="123" name="topping" value="Paneer" />
+                    <Form.Label>&nbsp;&nbsp;Project Team/Collaborators</Form.Label> <br />
+                    <input type="checkbox" id="234" name="topping" value="Paneer" />
+                    <Form.Label>&nbsp;&nbsp;Volunteers</Form.Label> <br />
+                    <input type="checkbox" id="345" name="topping" value="Paneer" />
+                    <Form.Label>&nbsp;&nbsp;Material donors</Form.Label> <br />
+                    <input type="checkbox" id="456" name="topping" value="Paneer" />
+                    <Form.Label>&nbsp;&nbsp;Idea suggestion</Form.Label> <br />
+                    <input type="checkbox" id="567" name="topping" value="Paneer" />
+                    <Form.Label>&nbsp;&nbsp;Specific Feedback</Form.Label> <br />
+                    <input type="checkbox" id="678" name="topping" value="Paneer" />
+                    <Form.Label>&nbsp;&nbsp;Other</Form.Label> <br />
+                    <Form.Control
+                      type='text'
+                      name='title'
+                      onChange={formik.handleChange}
+                      value={formik.values.title}
+                      placeholder="Describe other"
+                    />
+                  </div>
+              </div>
+            </Form.Group>
+            <Button
+              block
+              size="lg"
+              type='submit'
+              disabled={isLoading ? true : false}
+            >
+              {isLoading ? "Saving..." : "Submit your Direct Proposal"}
+          </Button>
+          </Form>
+
+          {error && (
+            <Alert variant='danger' className="error-alert">
+              { error.message}
+            </Alert>
+          )}
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default SubmitDirectProposalPageContent
--- frontend/src/components/ui/Header.tsx
+++ frontend/src/components/ui/Header.tsx
@@ -1,61 +1,75 @@
-import { useContext, useState } from 'react'
-import { NavDropdown, Nav, Navbar, Form, FormControl, Button } from 'react-bootstrap'
-import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
-import { useUserWithJwtVerbose } from 'src/hooks/userHooks';
-import { UserProfileContext } from '../../contexts/UserProfile.Context';
+import { useContext, useState } from "react";
+import {
+  NavDropdown,
+  Nav,
+  Navbar,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
+import { useUserWithJwtVerbose } from "src/hooks/userHooks";
+import { UserProfileContext } from "../../contexts/UserProfile.Context";
 export default function Header() {
-  const { logout, user, token} = useContext(UserProfileContext);
+  const { logout, user, token } = useContext(UserProfileContext);
   const { data } = useUserWithJwtVerbose({
     jwtAuthToken: token!,
-    shouldTrigger: token != null
+    shouldTrigger: token != null,
   });
   console.log(data);
   // Here Items are not coming Inline
   return (
     <div className="outer-header">
-      <Navbar className="inner-header" bg='light' expand='sm'>
-        <Navbar.Brand href='/'>
+      <Navbar className="inner-header" bg="light" expand="sm">
+        <Navbar.Brand href="/">
           <img
-            src='/MyLivingCityIcon.png'
-            width='30'
-            height='30'
-            className='d-inline-block alight-top'
+            src="/MyLivingCityIcon.png"
+            width="30"
+            height="30"
+            className="d-inline-block alight-top"
             alt="My Living City Logo"
           />
         </Navbar.Brand>
-        <Nav.Link href='/profile' className="d-inline-block alight-top">{data && (`${data.fname}@${data!.address!.streetAddress}`)}</Nav.Link>
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-        <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='ml-auto'>
-            <Nav.Link href='/'>Home</Nav.Link>
-            <Nav.Link href='/ideas'>Conversations</Nav.Link>
+        <Nav.Link href="/profile" className="d-inline-block alight-top">
+          {data && `${data.fname}@${data!.address!.streetAddress}`}
+        </Nav.Link>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/ideas">Conversations</Nav.Link>
             {user ? (
               <>
                 {/* <Navbar.Text> Fake Name</Navbar.Text> */}
-                <Nav.Link href='/submit'>Submit Idea</Nav.Link>
-                <Nav.Link href='/profile'>Profile</Nav.Link>
+                <Nav.Link href="/submit">Submit Idea</Nav.Link>
+                <Nav.Link href="/submit-direct-proposal">Submit Proposal</Nav.Link>
+                <Nav.Link href="/profile">Profile</Nav.Link>
 
-                {user.userType ==="ADMIN" &&
-                <NavDropdown title="Admin Tools" id="nav-dropdown">
-                <Nav.Link href='/advertisement/all'>Ads</Nav.Link>
-                <Nav.Link href='/segment/management'>Segments</Nav.Link>
-                <Nav.Link href='/user/management'>Users</Nav.Link>
-              </NavDropdown>
-                }
-                {user.userType ==="SEG_ADMIN" &&
-                <NavDropdown title="Seg-Admin Tools" id="nav-dropdown">
-                <Nav.Link href='/segment/management'>Segments</Nav.Link>
-              </NavDropdown>
-                }
-                {user.userType ==="MOD" &&
-                <NavDropdown title="Mod Tools" id="nav-dropdown">
-              </NavDropdown>
-                }
-                <Nav.Link href="https://mylivingcity.org/community-discussion-platform-help-pages/">Help</Nav.Link>
+                {user.userType === "ADMIN" && (
+                  <NavDropdown title="Admin Tools" id="nav-dropdown">
+                    <Nav.Link href="/advertisement/all">Ads</Nav.Link>
+                    <Nav.Link href="/segment/management">Segments</Nav.Link>
+                    <Nav.Link href="/user/management">Users</Nav.Link>
+                  </NavDropdown>
+                )}
+                {user.userType === "SEG_ADMIN" && (
+                  <NavDropdown title="Seg-Admin Tools" id="nav-dropdown">
+                    <Nav.Link href="/segment/management">Segments</Nav.Link>
+                  </NavDropdown>
+                )}
+                {user.userType === "MOD" && (
+                  <NavDropdown
+                    title="Mod Tools"
+                    id="nav-dropdown"
+                  ></NavDropdown>
+                )}
+                <Nav.Link href="https://mylivingcity.org/community-discussion-platform-help-pages/">
+                  Help
+                </Nav.Link>
                 <Nav.Link onClick={() => logout()}>Log out</Nav.Link>
               </>
             ) : (
-              <Nav.Link href='/login'>Login</Nav.Link>
+              <Nav.Link href="/login">Login</Nav.Link>
             )}
           </Nav>
         </Navbar.Collapse>
