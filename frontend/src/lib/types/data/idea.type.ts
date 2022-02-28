1df8bdecc5c487e5b3274ea66989b7f06f485a92
Maximus Joe <maximusjoe278@gmail.com>
2022-02-28 09:38:42 -0800

    Revert "Merge commit '1305fb61148c9ae4507fed9d8fa5d9ed3c76431a' into dev-max2"
    This reverts commit 00b69423c12b7ebf98e92b4a8d7a3a4323269d24, reversing
    changes made to 01a11b002bab82171ece44456ca82d034bd42358.

--- frontend/src/lib/types/data/idea.type.ts
+++ frontend/src/lib/types/data/idea.type.ts
@@ -1,5 +1,5 @@
 import { ICategory } from "./category.type";
-import { IAddress } from './address.type'
+import { IAddress } from "./address.type";
 import { IGeo } from "./geo.type";
 import { IProposal } from "./proposal.type";
 import { IProject } from "./project.type";
@@ -8,28 +8,28 @@ import { IComment } from "./comment.type";
 import { IUser } from "./user.type";
 import { ISegment, ISubSegment, ISuperSegment } from "./segment.type";
 
-export type IdeaState = 'IDEA' | 'PROPOSAL' | 'PROJECT';
+export type IdeaState = "IDEA" | "PROPOSAL" | "PROJECT";
 
 // Root Idea with no relationships
 export interface IIdea {
   id: number;
-	authorId: string;
-	championId: string;
-	categoryId: number;
-	segmentId: number; //
-	subSegmentId?: number; //
-	title: string;
-	description: string;
-	imagePath: string;
-	communityImpact?: string;
-	natureImpact?: string;
-	artsImpact?: string;
-	energyImpact?: string;
-	manufacturingImpact?: string;
-	state: IdeaState;
-	active: boolean;
-	createdAt: string;
-	updatedAt: string;
+  authorId: string;
+  championId: string;
+  categoryId: number;
+  segmentId: number; //
+  subSegmentId?: number; //
+  title: string;
+  description: string;
+  imagePath: string;
+  communityImpact?: string;
+  natureImpact?: string;
+  artsImpact?: string;
+  energyImpact?: string;
+  manufacturingImpact?: string;
+  state: IdeaState;
+  active: boolean;
+  createdAt: string;
+  updatedAt: string;
 }
 
 // Idea with relationships (Used in Single Idea Page) extends Root base Idea
@@ -39,43 +39,43 @@ export interface IIdeaWithRelationship extends IIdea {
   address?: IAddress;
   category?: ICategory;
   userType: string;
-  segment?: ISegment; // 
+  segment?: ISegment; //
   subSegment?: ISubSegment; //
   superSegment?: ISuperSegment;
   author?: IUser;
-	champion?: IUser | null;
-  proposalInfo?: IProposal | null;
-  projectInfo?: IProject | null;
-	
-	// Comments and Ratings are fetched seperately but could be fetched
-	comments?: IComment[];
-	ratings?: IRating[]
+  champion?: IUser | null;
 
-	// Checks to see if idea has met thresholds
-	isChampionable?: boolean;
+  projectInfo?: IProject | null;
+
+  // Comments and Ratings are fetched seperately but could be fetched
+  comments?: IComment[];
+  ratings?: IRating[];
+
+  // Checks to see if idea has met thresholds
+  isChampionable?: boolean;
 }
 
 // Idea Breakdown (Used in Landing Page and Ideas Page) extends Root base Idea
 export interface IIdeaWithAggregations {
-	id: number;
-	authorId: string;
-	categoryId: number;
-	segmentName: string; //
-	subSegmentName?: string; //
-	firstName: string; //
-	streetAddress: string; //
-	title: string;
-	description: string;
-	state: IdeaState;
-	active: boolean;
-	createdAt: string;
-	updatedAt: string;
+  id: number;
+  authorId: string;
+  categoryId: number;
+  segmentName: string; //
+  subSegmentName?: string; //
+  firstName: string; //
+  streetAddress: string; //
+  title: string;
+  description: string;
+  state: IdeaState;
+  active: boolean;
+  createdAt: string;
+  updatedAt: string;
 
-	// Aggregate breakdown
-	engagements: number;
-	ratingAvg: number;
-	commentCount: number;
-	ratingCount: number;
-	posRatings: number;
-	negRatings: number;
+  // Aggregate breakdown
+  engagements: number;
+  ratingAvg: number;
+  commentCount: number;
+  ratingCount: number;
+  posRatings: number;
+  negRatings: number;
 }
