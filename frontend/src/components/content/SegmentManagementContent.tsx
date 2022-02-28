d73fc238a1316b1fe91060d83d8cd33461c17c49
Maximus Joe <maximusjoe278@gmail.com>
2022-02-28 09:26:44 -0800

    Revert "Revert "Merge branch 'development' into dev-maximus""
    This reverts commit 1305fb61148c9ae4507fed9d8fa5d9ed3c76431a.

--- frontend/src/components/content/SegmentManagementContent.tsx
+++ frontend/src/components/content/SegmentManagementContent.tsx
@@ -116,8 +116,7 @@ export const ShowSubSegments: React.FC<ShowSubSegmentsProps> = ({
               <th>Name</th>
               <th>Lat</th>
               <th>Lon</th>
-              <th>Radius</th>
-              <th style={{ width: "10rem" }}>Controls</th>
+              <th>Controls</th>
             </tr>
           </thead>
           <tbody>
@@ -130,7 +129,6 @@ export const ShowSubSegments: React.FC<ShowSubSegmentsProps> = ({
                     </td>
                     <td>{segment.lat}</td>
                     <td>{segment.lon}</td>
-                    <td>{segment.radius}</td>
                     <td>
                       <NavDropdown title="Controls" id="nav-dropdown">
                         <Dropdown.Item
@@ -171,30 +169,14 @@ export const ShowSubSegments: React.FC<ShowSubSegmentsProps> = ({
                       />
                     </td>
                     <td>
-                      <Form.Control
-                        type="text"
-                        defaultValue={segment.radius}
-                        onChange={(e) => {
-                          segment.radius = parseFloat(e.target.value);
-                        }}
-                      />
-                    </td>
-                    <td>
                       <Button
-                        size="sm"
                         variant="outline-danger"
                         className="mr-2"
                         onClick={() => setHideControls("")}
                       >
                         Cancel
                       </Button>
-                      <Button
-                        size="sm"
-                        onClick={() => {
-                          handleSubSegSubmit(segment);
-                          setHideControls("");
-                        }}
-                      >
+                      <Button onClick={() => handleSubSegSubmit(segment)}>
                         Save
                       </Button>
                     </td>
@@ -223,14 +205,6 @@ export const ShowSubSegments: React.FC<ShowSubSegmentsProps> = ({
                     type="text"
                     onChange={(e) =>
                       (createData.lon = parseFloat(e.target.value))
-                    }
-                  ></Form.Control>
-                </td>
-                <td>
-                  <Form.Control
-                    type="text"
-                    onChange={(e) =>
-                      (createData.radius = parseFloat(e.target.value))
                     }
                   ></Form.Control>
                 </td>
