ae8d7e193211807b2c8575118e060b7bbd509bad
Maximus Joe <maximusjoe278@gmail.com>
2022-04-04 20:54:30 -0700

    created dasboard abstraction

--- frontend/src/components/content/DashboardPageContent.tsx
+++ frontend/src/components/content/DashboardPageContent.tsx
@@ -2,13 +2,12 @@ import React from "react";
 import { Container, Row, Spinner } from "react-bootstrap";
 import { IIdeaWithAggregations } from "../../lib/types/data/idea.type";
 import { IFetchError } from "../../lib/types/types";
-import CategoriesSection from "../partials/LandingContent/CategoriesSection";
-import DescriptionSection from "../partials/LandingContent/DescriptionSection";
-import HeroBannerSection from "../partials/LandingContent/HeroBannerSection";
 import NewAndTrendingSection from "../partials/LandingContent/NewAndTrendingSection";
 
 import AdsSection from "../partials/LandingContent/AdsSection"; //
 import { AdsSectionPage } from "src/pages/AdsSectionPage";
+import SystemMessages from "../partials/DashboardContent/SystemMessages";
+import SystemUpdates from "../partials/DashboardContent/SystemUpdates";
 
 interface LandingPageContentProps {
   topIdeas: IIdeaWithAggregations[] | undefined;
@@ -26,26 +25,32 @@ const DashboardPageContent: React.FC<LandingPageContentProps> = ({
   return (
     <Container className="landing-page-content">
       <Row as="article" className="featured"></Row>
+
+      <Row as="article" className="system-messages">
+        <SystemMessages />
+      </Row>
+
       {ideasLoading && (
         <div className="landing-spinner d-flex justify-content-center my-4">
           <Spinner animation="border" />
         </div>
       )}
+
       {topIdeas && !ideasIsError && !ideasLoading && (
         <Row as="article" className="new-and-trending">
-          <Container className="py-5" id="hanging-icons">
-            <h2 className="pb-1 border-bottom display-6 text-center">
-              System Messages
-            </h2>
-          </Container>
           <NewAndTrendingSection topIdeas={topIdeas!} />
-          <Container className="py-5" id="hanging-icons">
-            <h2 className="pb-1 border-bottom display-6 text-center">
-              Followed Posts and Updates
-            </h2>
-          </Container>
         </Row>
       )}
+
+      {ideasLoading && (
+        <div className="landing-spinner d-flex justify-content-center my-4">
+          <Spinner animation="border" />
+        </div>
+      )}
+
+      <Row as="article" className="system-updates">
+        <SystemUpdates topIdeas={topIdeas!} />
+      </Row>
     </Container>
   );
 };
