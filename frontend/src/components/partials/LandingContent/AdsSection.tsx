2b7773108379e7489a75686bbf47d16238dd5be4
Maximus Joe <maximusjoe278@gmail.com>
2022-04-23 23:19:15 -0700

    added default image

--- frontend/src/components/partials/LandingContent/AdsSection.tsx
+++ frontend/src/components/partials/LandingContent/AdsSection.tsx
@@ -1,35 +1,39 @@
-import {Container, Carousel } from 'react-bootstrap';
+import { Container, Carousel } from "react-bootstrap";
 // import { Slider } from 'infinite-react-carousel';
-import React from 'react';
-import { API_BASE_URL } from 'src/lib/constants';
-import { IAdvertisement } from 'src/lib/types/data/advertisement.type';
+import React from "react";
+import { API_BASE_URL } from "src/lib/constants";
+import { IAdvertisement } from "src/lib/types/data/advertisement.type";
 
 //http://madmartech.com/wp-content/uploads/2019/05/970x250-1.png
 //https://www.frontiersin.org/files/Articles/70413/fpsyg-05-00166-HTML/image_m/fpsyg-05-00166-g006.jpg
 interface AdsSectionProps {
-    ads: IAdvertisement[] | undefined;
+  ads: IAdvertisement[] | undefined;
 }
 
-const AdsSection: React.FC<AdsSectionProps> = ({ads}) => {
-    if(ads !== undefined){
-        return (
-            <Container>
-                {/* <h2 className="pb-1 border-bottom display-6 text-center">Our Locals</h2> */}
-    
-                <Carousel indicators={false} pause={'hover'}>
-                {ads.map(ad => 
-                <Carousel.Item interval={5000} key={ad.id}>
-                        <a href={ad.externalLink}>
-                            <img
-                            alt="Not found..."
-                            className="d-block w-100"
-                            src={`${API_BASE_URL}/${(ad.imagePath)}`}
-                            />
-                        </a>
-                </Carousel.Item>)}
-                    
-    
-                    {/* <Carousel.Item interval={5000}>
+const AdsSection: React.FC<AdsSectionProps> = ({ ads }) => {
+  if (ads !== undefined) {
+    return (
+      <Container>
+        {/* <h2 className="pb-1 border-bottom display-6 text-center">Our Locals</h2> */}
+        <Carousel indicators={false} pause={"hover"}>
+          {ads.map((ad) => (
+            <Carousel.Item interval={5000} key={ad.id}>
+              <a href={ad.externalLink}>
+                <img
+                  alt="Not found..."
+                  className="d-block w-100"
+                  style={{
+                    margin: "0 auto",
+                    maxHeight: "150px",
+                    maxWidth: "100%",
+                  }}
+                  src={`${API_BASE_URL}/${"uploads/adImage/image-not-found.jpg"}`}
+                />
+              </a>
+            </Carousel.Item>
+          ))}
+
+          {/* <Carousel.Item interval={5000}>
                         <a href='https://99designs.ca/blog/marketing-advertising/14-design-tips-for-more-clickable-banner-ads/'>
                             <img
                             className="d-block w-100"
@@ -37,13 +41,12 @@ const AdsSection: React.FC<AdsSectionProps> = ({ads}) => {
                             />
                         </a>
                     </Carousel.Item> */}
-                </Carousel>
-            </Container>
-        );
-    }else{
-        return(<></>)
-    }
-    
-}
+        </Carousel>
+      </Container>
+    );
+  } else {
+    return <></>;
+  }
+};
 
 export default AdsSection;
