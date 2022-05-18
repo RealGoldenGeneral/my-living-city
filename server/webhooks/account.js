b60f5a876ac8a378ddb4e82d90551e1f06eca5a3
Anson Chan <achan436@my.bcit.ca>
2022-05-18 00:54:03 -0700

    Created webhooks for Stripe to call when payment succeeded and when subscription is cancelled

--- server/webhooks/account.js
+++ server/webhooks/account.js
@@ -27,13 +27,60 @@ accountRouter.get(
 )
 
 accountRouter.post(
+    '/cancel',
+    async (req, res) => {
+        try {
+            const event = req.body;
+            let parsedStripeId;
+
+            switch (event.type) {
+                case 'customer.subscription.deleted':
+                    parsedStripeId = event.data.object.customer;
+                    console.log(`subscription deleted for stripe customer ${parsedStripeId}`);
+                    break;
+
+                default:
+                    return res.status(400).end();
+            }
+
+            const deleteStripeId = await prisma.userStripe.deleteMany({
+                where: {
+                    stripeId: parsedStripeId
+                }
+            })
+
+            res.status(200).json(deleteStripeId);
+
+        } catch (error) {
+            console.log(error);
+            res.status(400).end();
+        } finally {
+            await prisma.$disconnect();
+        }
+    }
+)
+
+accountRouter.post(
     '/subscribe',
     async (req, res) => {
         try {
+            const event = req.body;
+            let stripeId;
+
+            switch (event.type) {
+                case 'payment_intent.succeeded':
+                    stripeId = event.data.object.customer;
+                    console.log(`PaymentIntent success for customer ${stripeId}`);
+                    break;
+                default:
+                    return res.status(400).end();
+            }
+
             let createUserStripe = await prisma.userStripe.create({
                 data: {
                     userId: 'cl2nyodqn0004psun25iapajh',
-                    stripeId: '890abc',
+                    stripeId: stripeId,
+                    status: 'ACTIVE'
                 }
             })
 
