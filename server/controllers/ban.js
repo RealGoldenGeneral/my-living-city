a6ad0173026cdc17a4bb8430825be054594c7471
Connor Young <connor-young@outlook.com>
2022-10-29 13:45:17 -0700

    implemented /create route

--- server/controllers/ban.js
+++ server/controllers/ban.js
@@ -5,55 +5,51 @@ const prisma = require('../lib/prismaClient');
 //WIP; need to add passport.authenticate(token) after testing
 
 banRouter.post(
-    '/create', async (req, res) => {
-        const days = 30;
-        await prisma.ban.create({
-            data: {
-                userId: 'cl9q1pnx20003i0edgynd9la0',
-                banUntil: new Date(Date.now() + (days * 24 * 60 * 60 * 1000)),
-                banMessage: "message",
-                banReason: "reason",
-                isWarning: true,
-            }
-        })
-        res.json("ban created");
-    }
-)
-
-banRouter.post(
-    '/create/:userId',
-    bodyParser.json(),
+    '/create',
     async (req, res) => {
-        const foundUser = await prisma.user.findUnique({ where: { id: req.params.userId } });
-        if (!foundUser) {
-            return res.status(400).json({
-                message: `The comment with that listed ID (${req.params.userId}) does not exist.`,
-            });
-        }
-        // Check if user is already banned
-        const userAlreadyBanned = await prisma.ban.findFirst({
-            where: {
-                userId: req.params.userId
+        try {
+            const foundUser = await prisma.user.findUnique({ where: { id: req.body.userId } });
+            if (!foundUser) {
+                return res.status(400).json({
+                    message: `The comment with that listed ID (${req.params.userId}) does not exist.`,
+                });
             }
-        });
-        if (userAlreadyBanned) {
-            return res.status(400).json({
-                message: `This user is already banned.`,
+            // Check if user is already banned
+            const userAlreadyBanned = await prisma.ban.findFirst({
+                where: {
+                    userId: req.params.userId
+                }
+            });
+            if (userAlreadyBanned) {
+                return res.status(400).json({
+                    message: `This user is already banned.`,
+                    details: {
+                        errorMessage: "A user can only be banned once."
+                    }
+                });
+            }
+            const createdBan = await prisma.ban.create({
+                data: {
+                    userId: req.body.userId,
+                    banUntil: new Date(Date.now() + (req.body.banDuration * 24 * 60 * 60 * 1000)),
+                    banMessage: req.body.banMessage,
+                    banReason: req.body.banReason,
+                    isWarning: req.body.isWarning,
+                }
+            });
+            res.status(200).json({
+                message: `Successfully banned user ${req.body.userId}`,
+                createdBan
+            });
+        } catch (error) {
+            res.status(400).json({
+                message: `Error occurred when trying to ban user ${req.body.userId}`,
                 details: {
-                    errorMessage: "A user can only be banned once."
+                    errorMessage: error.message,
+                    errorStack: error.stack,
                 }
             });
         }
-        const createdBan = await prisma.ban.create({
-            data: {
-                userId: req.params.userId,
-                banUntil: new Date(Date.now() + (req.body.banDuration * 24 * 60 * 60 * 1000)),
-                banMessage: req.body.banMessage,
-                banReason: req.body.banReason,
-                isWarning: req.body.isWarning,
-            }
-        });
-        res.json("ban created");
     }
 )
 
