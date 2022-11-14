2d1d8f9e9d4780ecf19693ccdbef033b56b53730
penguinasian <zhangqiangtianchen@gmail.com>
2022-11-13 16:48:18 -0800

    added migration folders

---
+++ server/prisma/migrations/20221103185518_breakdown_proposal_description/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - Added the required column `proposal_benefits` to the `idea` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `proposal_goal` to the `idea` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `proposal_requirements` to the `idea` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `proposal_role` to the `idea` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "idea" ADD COLUMN     "proposal_benefits" TEXT NOT NULL,
+ADD COLUMN     "proposal_goal" TEXT NOT NULL,
+ADD COLUMN     "proposal_requirements" TEXT NOT NULL,
+ADD COLUMN     "proposal_role" TEXT NOT NULL;
---
+++ server/prisma/migrations/20221103195138_modify_idea_table_proposal_breakdown/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `proposal_requirements` on the `idea` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "idea" DROP COLUMN "proposal_requirements";
