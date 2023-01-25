b84aedb81342d171add722570f1b006e417fc889
Owen George <ogeorge5@my.bcit.ca>
2023-01-24 18:26:20 -0800

    Added quarantine_notifications table to prisma schema

--- server/controllers/dashboard.js
+++ server/controllers/dashboard.js
@@ -3,4 +3,23 @@ const express = require('express');
 const prisma = require('../lib/prismaClient');
 const dashboardRouter = express.Router();
 
+dashboardRouter.get(
+  '/test',
+  async (req, res, next) => {
+    try {
+      const test = await prisma.quarantine_Notifications.findMany();
+      res.send(test);
+    }
+    catch (error) {
+      res.status(400).json({
+        message: error.message,
+        details: {
+          errorMessage: error.message,
+          errorStack: error.stack,
+        }
+      });
+    }
+  }
+)
+
 module.exports = dashboardRouter;
